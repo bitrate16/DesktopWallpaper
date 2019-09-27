45262e3931f7fcdc9d424a29795c8f53417c5263
Eytlin <dartlevonchic@gmail.com>
2019-09-27 23:34:19 +0300

    Create fragment_shader.glsl

---
+++ Build/x86/shaders/noise/fragment_shader.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+
+uniform float iTime;
+uniform vec3 iResolution;
+#define fragCoord gl_FragCoord.xy
+
+out vec4 fragColor;
+
+float noise(vec2 p) {
+    return fract(dot(sin(p.x * 123.12)*142.,cos(p.y *34.95)*165.47));
+}
+
+void main() {
+	vec2 uv = fragCoord.xy / iResolution.xy ;
+    float n = noise(uv + iTime);
+	fragColor = vec4(n,n,n,1.0);
+}
